5cf659fb04dcfba1e656e7237c21860733da7bc5
Mehraveh-Nejabati42 <mehraveh.nejabati42@gmail.com>
2025-07-21 19:32:25 +0330

    comment added

--- timer.py
+++ timer.py
@@ -4,6 +4,6 @@ for h in range(0,24):
     for m in range(0,60):
         for s in range(0,60):
             clock=f"{h}:{m}:{s}"
-            t.write(clock,font=('segoe print',60))
+            t.write(clock,font=('segoe print',60)) #test
             time.sleep(1)
             t.clear()
